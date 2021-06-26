4061883b5f84edb6991cda0e7e5bb514bd2508c1
ZitronCrazy <zitro5@protonmail.com>
2021-06-26 23:31:00 +0200

    NumberPalindrome

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        NumberPalindrome.isPalindrome(-1221);
+        NumberPalindrome.isPalindrome(707);
+        NumberPalindrome.isPalindrome(11212);
+    }
+}
